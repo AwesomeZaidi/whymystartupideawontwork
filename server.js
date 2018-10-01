ea644932e2229ff2edb0e4b7221430f46b2e8bb8
Asim Zaidi <asimzaidih@gmail.com>
2018-10-01 10:47:54 -0700

    users can now see ideas at root route

--- server.js
+++ server.js
@@ -5,9 +5,20 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.get('/', (req, res) => {
-    res.render('home', { msg: 'Hello World!' });
-})
+// app.get('/', (req, res) => {
+//     res.render('home', { msg: 'Hello World!' });
+// })
+
+// OUR MOCK ARRAY OF PROJECTS
+let ideas = [
+    { pitch: "LinkedIn for dogs." },
+    { pitch: "Uber for plumbers!" }
+  ]
+  
+  // INDEX
+  app.get('/', (req, res) => {
+    res.render('ideas-index', { ideas: ideas });
+  })
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
